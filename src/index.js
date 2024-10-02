8fc8794b58546c376ee5d6fd9ee4eafe317a1e23
suraj5336 <135395937+suraj5336@users.noreply.github.com>
2024-10-02 18:43:42 +0530

    recent change

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 // require("dotenv").config({ path: "./env" });
 
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-const app = express();
-
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("Mongodb connection failed !!! ", err);
+  });
 
 /*
     (async () => {
